56c444b270cc7c5f1928589ba0c86c8ab6275026
sachingitpreneurpune <saching.itpreneurpune@gmail.com>
2026-05-04 16:33:54 +0530

    first

---
+++ JavaScript/ImportExport/data.js
@@ -0,0 +1,18 @@
+let name="Ashish";
+export default name; //single
+
+let a=10
+let b=20;
+
+
+let user={
+    username:"Aman",
+    age:23
+}
+
+
+const greet=()=>{
+    alert("Hello")
+}
+
+export {a,b,user,greet};
---
+++ JavaScript/ImportExport/script.js
@@ -0,0 +1,10 @@
+
+// import name from './data.js'
+// alert("Hello Welcome "+name)
+
+import {a,b,user,greet} from './data.js'
+// alert(a+b)
+console.log(user)
+
+
+document.getElementById("btn").addEventListener("click",greet)
